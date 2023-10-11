c6ca2dd34c188579b3e59755f9aa4173bc6c98a8
Nihal <nihal@mislbd.com>
2023-10-11 12:01:41 +0600

    Get page 2 of all users

---
+++ src/test/java/HTTPRequests.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class HTTPRequests {
+
+    @Test
+    void getUsers(){
+        given()
+
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+
+                .then()
+                .statusCode(200)
+                .body("page", equalTo(2))
+                .log().all();
+
+    }
+}
