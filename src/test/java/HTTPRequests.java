2ad07de419cf47f7281938e1c7b4700528b13c4e
Nihal <nihal@mislbd.com>
2023-10-11 15:09:47 +0600

    create,update,delete users added

--- src/test/java/HTTPRequests.java
+++ src/test/java/HTTPRequests.java
@@ -1,12 +1,16 @@
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class HTTPRequests {
+    int id;
 
-    @Test
-    void getUsers(){
+    @Test(priority = 1)
+    void getUsers() {
         given()
 
                 .when()
@@ -16,6 +20,57 @@ public class HTTPRequests {
                 .statusCode(200)
                 .body("page", equalTo(2))
                 .log().all();
+    }
+
+    @Test(priority = 2)
+    void createUser() {
+
+        HashMap data = new HashMap<>();
+        data.put("name", "Nihal");
+        data.put("job", "QA");
+        id = given()
+                .contentType("application/json")
+                .body(data)
+
+                .when()
+                .post("https://reqres.in/api/users")
+                .jsonPath().getInt("id");
+
 
     }
+
+    @Test(priority = 3, dependsOnMethods = {"createUser"})
+        // dependsonMethods is this method will be executed if the mentioned method/test is passed
+    void updateUser() {
+
+        HashMap updata = new HashMap<>();
+        updata.put("name", "Nishat");
+        updata.put("job", "Dev");
+
+        given()
+                .contentType("application/json")
+                .body(updata)
+
+                .when()
+                .put("https://reqres.in/api/users/" + id)
+
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+    @Test(priority = 4)
+    void deleteUser(){
+        given()
+
+                .when()
+                .delete("https://reqres.in/api/users/" + id)
+
+                .then()
+                .statusCode(204)
+                .log().all();
+    }
 }
+
+
